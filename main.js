c2417e84725d0b3c60f465c6ed03191b0d26ded3
Andrew Musselman <andrew.mussels@gmail.com>
2022-01-17 16:05:14 -0700

    Instantiate new game on page load

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
+var currentGame;
+//query Selectors
 var gameBoard = document.querySelector('.game-board');
 var playerTurn = document.querySelector('.player-turn');
+
+// event Listeners
+window.addEventListener('load', createCurrentGame);
+
+// event handlers
+function createCurrentGame() {
+  currentGame = new Game();
+}
